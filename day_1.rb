7dcad6bf86e87c5c32b9a0636112714ee3b7f460
Pawel Duda <pawel.duda91@gmail.com>
2019-12-01 23:10:57 +0100

    Add solution for day 1, part 2

--- day_1.rb
+++ day_1.rb
@@ -1,3 +1,5 @@
+# https://adventofcode.com/2019/day/1
+
 class FuelCalculator
   attr_reader :modules_mass
 
@@ -16,17 +18,13 @@ class FuelCalculator
   end
 
   def fuel_for_mass(mass)
-    # Fuel required to launch a given module is based on its mass.
-    # Specifically, to find the fuel required for a module,
-    # - take its mass,
-    # - divide by three,
-    # - round down,
-    # - and subtract 2.
-    (mass / 3).floor - 2
+    fuel = (mass / 3).floor - 2
+    (fuel <= 0) ? 0 : (fuel + fuel_for_mass(fuel))
   end
 end
 
-p FuelCalculator.new([12, 14, 1969, 100756]).call == [2, 2, 654, 33583].reduce(:+)
+p FuelCalculator.new([1969]).call == 966
+p FuelCalculator.new([100756]).call == 50346
 
 modules_mass = File.open('day_1_input.txt').readlines.map(&:chomp).map(&:to_i)
 p FuelCalculator.new(modules_mass).call
