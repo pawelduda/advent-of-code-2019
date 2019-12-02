3e737b1377e28ecbfaaaa7bde51480377b90f4d3
Pawel Duda <pawel.duda91@gmail.com>
2019-12-02 22:33:38 +0100

    Complete day 2, part 2

--- day_2.rb
+++ day_2.rb
@@ -3,8 +3,10 @@ class IntcodeProcessor
   MULT_OPTCODE = 2
   HALT_OPTCODE = 99
 
-  def initialize(intcode)
+  def initialize(intcode, noun, verb)
     @intcode = intcode
+    @noun = noun
+    @verb = verb
   end
 
   def call
@@ -19,8 +21,8 @@ class IntcodeProcessor
   attr_accessor :position
 
   def restore_gravity_assist_program
-    @intcode[1] = 12
-    @intcode[2] = 2
+    @intcode[1] = @noun
+    @intcode[2] = @verb
   end
 
   def process_intcode
@@ -53,12 +55,20 @@ class IntcodeProcessor
   end
 end
 
+input = File.read('day_2_input.txt').chomp.split(',').map(&:to_i)
+
 # p IntcodeProcessor.new([1,0,0,0,99]).call == [2,0,0,0,99]
 # p IntcodeProcessor.new([2,3,0,3,99]).call == [2,3,0,6,99]
 # p IntcodeProcessor.new([2,4,4,5,99,0]).call == [2,4,4,5,99,9801]
 # p IntcodeProcessor.new([1,1,1,4,99,5,6,0,99]).call == [30,1,1,4,2,5,6,0,99]
 
-input = File.read('day_2_input.txt').chomp.split(',').map(&:to_i)
-
-p IntcodeProcessor.new(input).call
-
+# Part 2
+(0..99).each do |noun|
+  (0..99).each do |verb|
+    result = IntcodeProcessor.new(input.dup, noun, verb).call
+    if result[0] == 19690720
+      p 100 * noun + verb
+      exit
+    end
+  end
+end
