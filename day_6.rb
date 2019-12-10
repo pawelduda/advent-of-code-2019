8717b2391e5558160c043df14656d71f5db695ad
Pawel Duda <pawel.duda91@gmail.com>
2019-12-10 23:00:45 +0100

    Complete day 6, part 2

--- day_6.rb
+++ day_6.rb
@@ -9,7 +9,12 @@ class OrbitsCounter
 
   def call
     prepare_data
-    count_orbits
+
+    # Part 1
+    # count_orbits
+
+    # Part 2
+    shortest_path_to('YOU', 'SAN')
   end
 
   private
@@ -43,6 +48,31 @@ class OrbitsCounter
 
     count
   end
+
+  # Part 2
+  def shortest_path_to(obj1, obj2)
+    obj1_path = path_to_root_with_costs(obj1)
+    obj2_path = path_to_root_with_costs(obj2)
+    common_parents = obj1_path.keys & obj2_path.keys
+
+    common_parents.map do |parent|
+      [parent, obj1_path[parent] + obj2_path[parent]]
+    end.min { |parent| parent[1] }
+  end
+
+  def path_to_root_with_costs(object)
+    [].tap do |path|
+      cost = 0
+      current_object = orbits[object]
+
+      while orbits[current_object]
+        path << [current_object, cost]
+
+        cost += 1
+        current_object = orbits[current_object]
+      end
+    end.to_h
+  end
 end
 
 test_input = <<HEREDOC
@@ -57,9 +87,12 @@ D)I
 E)J
 J)K
 K)L
+K)YOU
+I)SAN
 HEREDOC
 
-p OrbitsCounter.new(test_input).call == 42
+# p OrbitsCounter.new(test_input).call == 42
+p OrbitsCounter.new(test_input).call == 4
 
 input = File.open('day_6_input.txt').read
 p OrbitsCounter.new(input).call
